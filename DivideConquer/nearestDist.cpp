ed05b0a9dc03067f7c8126c334a41708fd6a80b6
Redleaf23477 <schpokeool@gmail.com>
2018-07-19 10:35:47 +0800

    nearestDist updated

--- DivideConquer/nearestDist.cpp
+++ DivideConquer/nearestDist.cpp
@@ -1,38 +1,38 @@
-bool cmp_y(P &a, P &b)  // y increasing
+bool cmp_y(P a, P b)
 {
     return a.y < b.y;
 }
 
-bool cmp_x(P &a, P &b) // x increasing
+bool cmp_x(P a, P b)
 {
     return a.x < b.x;
 }
 
-void init()
+double dc(P *arr, int n)
 {
-    sort(arr, arr+n, cmp_y);
+    if(n == 1) return INF;
+    int mid = n/2;
+    double cx = arr[mid].x;
+    double dist = min( dc(arr, mid), dc(arr+mid, n-mid) );
+    inplace_merge(arr, arr+mid, arr+n, cmp_y);
+    static vector<P> brr; brr.clear();
+    for(int i = 0; i < n; i++)
+    {
+        if(fabs(arr[i].x)-cx >= dist) continue;
+        for(int j = brr.size()-1; j >= 0; j--)
+        {
+            double dx = brr[j].x-arr[i].x;
+            double dy = brr[j].y-arr[i].y;
+            if(fabs(dy) >= dist) break;
+            dist = min(dist, sqrt(dx*dx+dy*dy));
+        }
+        brr.push_back(arr[i]);
+    }
+    return dist;
 }
 
-double nearestDist(int l, int r)
+double nearestDist(P *arr, int n)
 {
-    if(l == r) return (double)INT_MAX;
-    int mid = (l+r)/2;
-    double d = min(dc(l, mid), dc(mid+1, r));
-    sort(arr+l, arr+r+1, cmp_x);
-    double center = arr[mid].x;
-    vector<P> cen;
-    for(int i = l; i <= r; i++)
-    {
-        if(fabs(arr[i].x-center) >= d) continue;
-        for(auto p:cen)
-        {
-            double dx = fabs(arr[i].x-p.x), dy = fabs(arr[i].y-p.y);
-            if(dy < d)
-            {
-                d = min(d, sqrt(dx*dx+dy*dy));
-            }
-        }
-        cen.push_back(arr[i]);
-    }
-    return d;
+    sort(arr, arr+n, cmp_x);
+    return dc(arr, n);
 }
