5dff61b9cc2b99baa964252eccc72a353c8e72c9
Lightbulb <pierre5340@gmail.com>
2019-05-31 19:48:06 +0800

    move to brain

--- Math/prime_detect.cpp
+++
@@ -1,8 +0,0 @@
-const int N = 10000000;
-bool isprime[N] = {true};
-void prime_detect()
-{
-    for(int i = 2; i < sq; i++) 
-        if(isprime[i]) 
-            for(int j = i*i; j < N; j+=i) isprime[j] = false;
-}
