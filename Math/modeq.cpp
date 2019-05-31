5f80b94de419fba2701e6ac74f90d6133d164ba8
Lightbulb <pierre5340@gmail.com>
2019-05-31 19:46:36 +0800

    rewrite style & comments

--- Math/modeq.cpp
+++ Math/modeq.cpp
@@ -1,20 +1,15 @@
-// 解線性模方程組(最小非負整數解)
-const int N;  //N個方程
-ll A[N], B[N], M[N];  // A * X = B (%M)
-ll solve() //解X, return INF if no solution 
+ll solve(ll *a, ll *b, ll *m, int N)
 {
-   ll k = 0, h = 1;
-   for(ll i = 0; i < N; i++)
-   {
-      ll a = A[i]*h, b = B[i]-A[i]*k, m = M[i], ar;
-      ll d = exgcd(a, ar=1, m); 
-      if(b%d != 0) return INF;
-      ll n = abs(m/d);
-      ll t = ar*b/d; t%=n; t+=n; t%=n;
-
-      k += h*t, h *= n; k%=h; //維護解是正的
-   }
-   int ret = (k%h+h)%h;
-   return ret;
+	ll k = 0, h = 1, gcd, n, t, ar;
+	for (ll i = 0; i < N; i++)
+	{
+		gcd = extgcd(a[i] * h, m[i], ar, t);
+		t = (b[i] - a[i] * k) / gcd, n = abs(m[i] / gcd);
+		if (t * gcd != b[i] - a[i] * k) return -1;
+		t = ((ar * t) % n + n) % n;
+		k += h * t, h *= n, k %= h;
+	}
+	return (k % h + h) % h;
 }
-
+// 解n組a*x=b%m, 回傳x.
+// 回傳的是最小非負整數解. 無解回傳-1.
