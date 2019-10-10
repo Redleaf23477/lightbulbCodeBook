e85a79d55fb3708c4fd6d25cf8eaca8a7c049865
lightbulb12294 <lightbulb@yuyuko.moe>
2019-10-10 15:32:06 +0800

    [Graph] HLD overhaul

--- Graph/HeavyLightDecomposition.cpp
+++ Graph/HeavyLightDecomposition.cpp
@@ -1,48 +1,49 @@
-const int MAX_N;
-vector<int> link[MAX_N]; //edge
-
-void dfs_build(int now, int fa, int *weight, int *depth, int *pa, int *son){
-	weight[now]=1;
-	son[now]=-1;
-	pa[now]=fa;
-	for(auto i:link[now])
-	{
-		if(i==fa) continue;
-		depth[i]=depth[now]+1;
-		dfs_build(i,now,weight,depth,pa,son);
-		if(son[now]==-1||weight[son[now]]<weight[i]) son[now]=i;
-		weight[now]+=weight[i];
+// 以樹重切樹鏈
+struct HLD {
+	static int MAX_N = 1e6+6;
+	vector<int> G[MAX_N];
+	int root=1; // set root!
+	int pa[MAX_N], son[MAX_N], d[MAX_N], w[MAX_N], link_top[MAX_N];
+	void add_edge(int x, int y) {
+		G[x].push_back(y);
+		G[y].push_back(x);
 	}
-}
-void build_top(int now, int top,int *pa, int *son, int *link_top){
-	link_top[now]=top;
-	if(son[now]==-1) return;
-	build_top(son[now],top,pa,son,link_top);
-	for(auto i:link[now])
-	{
-		if(i==son[now]||i==pa[now]) continue;
-		build_top(i,i,pa,son,link_top);
-	}
-}
-inline void HLD(int *weight, int *depth, int *pa, int *son, int *link_top){
-	memset(son,-1,sizeof(int)*MAX_N);
-	depth[1]=1; //set node(1) as root
-	dfs_build(1,0,weight,depth,pa,son);
-	build_top(1,1,pa,son,link_top);
-}
-inline int find_lca(int x, int y, int *depth, int *pa, int *link_top){
-	int tx=link_top[x], ty=link_top[y];
-	while(tx!=ty)
-	{
-		if(depth[tx]<depth[ty])
-		{
-			swap(tx,ty);
-			swap(x,y);
+	void build_dfs(int now, int P) {
+		w[now]=1;
+		son[now]=-1;
+		pa[now]=P;
+		for(auto &i:G[now]) {
+			if(i==P) continue;
+			d[i]=d[now]+1;
+			build_dfs(i,now);
+			if(~son[now]||w[son[now]]<w[i]) son[now]=i;
+			w[now]+=w[i];
 		}
-		tx=link_top[x=pa[x]];
 	}
-	return depth[x]<depth[y]?x:y;
-}
-//usage: 
-//build HeavyLightDecomposition: HLD
-//find LCA(x,y): find_lca
+	void build_top(int now, int top) {
+		link_top[now]=top;
+		if(~son[now]) return;
+		build_top(son[now],top);
+		for(auto &i:G[now]) {
+			if(i==pa[now]||i==son[now]) continue;
+			build_top(i,i);
+		}
+	}
+	void build() { // HLD build function
+		d[root]=1;
+		son[root]=pa[root]=-1;
+		build_dfs(root,-1);
+		build_top(root,root);
+	}
+	void find_lca(int x, int y) {
+		int tx=link_top[x], ty=link_top[y];
+		while(tx!=ty) {
+			if(d[tx]<d[ty]) {
+				swap(tx,ty);
+				swap(x,y);
+			}
+			tx=link_top[x=pa[x]];
+		}
+		return d[x]<d[y]?x:y;
+	}
+};
